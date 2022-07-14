17adf107246f542040cf57ebe28970784ddc42fe
rauenah <rauena.silvasousa@gmail.com>
2022-07-14 13:11:41 -0300

    Conceituação e criação

---
+++ src/one/digital/innovation/Main.java
@@ -0,0 +1,57 @@
+package one.digital.innovation;
+
+/**
+ *Classe de exemplo para o exercicio de Aula I de Variaveis, Tipos de Dados e Operadores arirméticos
+ */
+
+
+public class Main {
+
+    public  static void main(String[]args) {
+       int i;
+       //int i;
+       int I;
+       //int _1a;
+       int _1a;
+       int $aq;
+
+        i =5;
+        I=10;
+        _1a=20;
+        $aq= 7;
+
+        final int j =10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn2$4678_md;
+        //int asrn2$46%78_md =10;
+
+        asrn24678md=100;
+        asrn2$4678_md=10;
+
+        int quantidadeproduto=50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS=5;
+        //final int numeroTentativas=5;
+        int QUANTIDADE_OPCOES=25;
+        //int qtdPProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn2$4678_md);
+
+        System.out.println(quantidadeproduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+
+    }
+}
+
